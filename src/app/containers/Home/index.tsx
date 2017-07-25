e7df404f1e1746d364360a94b305c95feee1799c
Ray Matos <matos.ray@gmail.com>
2017-07-25 11:38:28 -0400

    add reactstrap

--- src/app/containers/Home/index.tsx
+++ src/app/containers/Home/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
+import { Button } from 'reactstrap';
 const style = require('./style.css');
 
 class Home extends React.Component<any, any> {
   public render() {
     return (
-      <div className={style.Home}>
-        <p>Hello!</p>
+      <div>
+        <div className={style.Home}>
+          <p>Hello!</p>
+        </div>
+        <div>
+          <Button color="primary">primary</Button>{' '}
+          <Button color="secondary">secondary</Button>{' '}
+          <Button color="success">success</Button>{' '}
+          <Button color="info">info</Button>{' '}
+          <Button color="warning">warning</Button>{' '}
+          <Button color="danger">danger</Button>{' '}
+          <Button color="link">link</Button>
+        </div>
       </div>
     );
   }
